0f86f64bd8cbc50c0cadc9c1723ff1b99bc8be81
Santiago <46566240+santiagoruizmoreno@users.noreply.github.com>
2021-09-19 12:18:55 +0200

    Add files via upload

---
+++ code_java.js
@@ -0,0 +1,150 @@
+var elem = document.querySelector('.grid');
+var iso = new Isotope( elem, {
+  // options
+  itemSelector: '.grid-item',
+  layoutMode: 'fitRows'
+});
+
+// element argument can be a selector string
+//   for an individual element
+var iso = new Isotope( '.grid', {
+  // options
+});
+
+
+function chbg(arg1,arg2) {
+    document.getElementById('principal').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+
+}   
+
+function chb2(arg1,arg2) {
+    document.getElementById('principal').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+function chbg1(arg1,arg2) {
+    document.getElementById('img2').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+
+}   
+
+function chb12(arg1,arg2) {
+    document.getElementById('img2').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+function chbg2(arg1,arg2) {
+    document.getElementById('img3').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+
+}   
+
+function chb22(arg1,arg2) {
+    document.getElementById('img3').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb31(arg1,arg2) {
+    document.getElementById('img4').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+function chb32(arg1,arg2) {
+    document.getElementById('img4').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb41(arg1,arg2) {
+    document.getElementById('img5').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb42(arg1,arg2) {
+    document.getElementById('img5').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb51(arg1,arg2) {
+    document.getElementById('img6').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb52(arg1,arg2) {
+    document.getElementById('img6').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb61(arg1,arg2) {
+    document.getElementById('img7').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb62(arg1,arg2) {
+    document.getElementById('img7').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb71(arg1,arg2) {
+    document.getElementById('img8').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb72(arg1,arg2) {
+    document.getElementById('img8').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb81(arg1,arg2) {
+    document.getElementById('img9').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb82(arg1,arg2) {
+    document.getElementById('img9').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb91(arg1,arg2) {
+    document.getElementById('img10').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb92(arg1,arg2) {
+    document.getElementById('img10').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb101(arg1,arg2) {
+    document.getElementById('img11').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb102(arg1,arg2) {
+    document.getElementById('img11').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb111(arg1,arg2) {
+    document.getElementById('img12').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb112(arg1,arg2) {
+    document.getElementById('img12').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+function chb121(arg1,arg2) {
+    document.getElementById('img13').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+function chb122(arg1,arg2) {
+    document.getElementById('img13').style.display = arg1;
+    document.getElementById('img1').style.display = arg2;
+}   
+
+
+
